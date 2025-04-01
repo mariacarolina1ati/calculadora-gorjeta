0bbb993a9003d15fd0fd2d9b665ce2ba279a9061
mcarolinadiasrodrigues <mcarolinadiasrodrigues@gmail.com>
2025-04-01 23:35:12 +0000

    criação da logica de selecionar um botão de gorjeta ou colocar um valor customizado

--- main.js
+++ main.js
@@ -14,10 +14,30 @@ function receiveNumberOfPeopleValue() {
 function receiveTipPercentageValue(value) {
     tipPercentage = value / 100
 
+    
+    document.querySelector("#custom-tip").value = ""
+    
+
     if(buttonSelected !== null) {
         buttonSelected.classList.remove("button-selected")
     }
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+
+}
+
+function receiveCustomTipPercentageValue() {
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+
+    removeClassButtonSelected()
+    
+}
+
+function removeClassButtonSelected() {
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected.style.backgroundColor = ""
+        buttonSelected = null
+    }
 }
